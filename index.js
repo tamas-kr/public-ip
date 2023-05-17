f9aa520cb34f7f250accfa0d467bd419d98cd728
Tamas <91263611+tamas-kr@users.noreply.github.com>
2023-05-17 14:49:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const ip = "1.2.3.4";
     const url = core.getInput('url');
+    let ip = "";
+    fetch(url)
+        .then(response => response.text())
+        .then(text => ip = text);
     console.log(`Service URL: ${url}`);
     console.log(`IP: ${ip}`);
     core.setOutput("ip", ip);
